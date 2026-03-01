03804bd68404c1049a0bcbfe6570eb471862a4ad
lutvzn <tech@lutvzn.com>
2026-03-01 11:55:38 -0300

    Updated configs

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -3,7 +3,7 @@ set -g fish_greeting
 if status is-interactive
     # Commands to run in interactive sessions can go here
     /home/lutvzn/.local/share/fnm/fnm env --use-on-cd --shell fish | source
-    ~.nix-profile/bin/starship init fish | source
+    ~/.nix-profile/bin/starship init fish | source
 end
 ### bling.fish source start
 test -f /usr/share/bazzite-cli/bling.fish && source /usr/share/bazzite-cli/bling.fish
