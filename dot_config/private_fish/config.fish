af1a2856e1e3c72002c13273d8d0f8083c3c1601
lutvzn <106083845+lutvzn@users.noreply.github.com>
2026-03-01 12:34:11 -0300

    Make configs point nix profile instead of linuxbrew

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -2,8 +2,9 @@ set -g fish_greeting
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    fish_add_path ~/.nix-profile/bin
     /home/lutvzn/.local/share/fnm/fnm env --use-on-cd --shell fish | source
-    ~/.nix-profile/bin/starship init fish | source
+    starship init fish | source
 end
 ### bling.fish source start
 test -f /usr/share/bazzite-cli/bling.fish && source /usr/share/bazzite-cli/bling.fish
