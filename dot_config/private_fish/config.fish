54a2365cfc6e546b93c1bc448cf3ea2e1996266f
lutvzn <106083845+lutvzn@users.noreply.github.com>
2026-03-07 10:20:40 -0300

    Updated fish and bashrc

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -6,6 +6,3 @@ if status is-interactive
     /home/lutvzn/.local/share/fnm/fnm env --use-on-cd --shell fish | source
     starship init fish | source
 end
-### bling.fish source start
-test -f /usr/share/bazzite-cli/bling.fish && source /usr/share/bazzite-cli/bling.fish
-### bling.fish source end
