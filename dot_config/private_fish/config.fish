00dd50d532ed3934f09004cb6f2f823f0ea9998a
lutvzn <106083845+lutvzn@users.noreply.github.com>
2026-03-12 20:42:39 -0300

    Added cli tools to configs

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,8 +1,30 @@
 set -g fish_greeting
 
 if status is-interactive
-    # Commands to run in interactive sessions can go here
-    fish_add_path ~/.nix-profile/bin
-    /home/lutvzn/.local/share/fnm/fnm env --use-on-cd --shell fish | source
-    starship init fish | source
+    # Prepend user bin and Nix directories
+    fish_add_path ~/.local/bin ~/bin
+    if test -d ~/.nix-profile/bin
+        fish_add_path ~/.nix-profile/bin
+    end
+
+    # Tool Initialization
+    if type -q zoxide; zoxide init fish | source; end
+    if type -q starship; starship init fish | source; end
+    if type -q atuin; atuin init fish | source; end
+    if type -q fzf; fzf --fish | source; end
+    if type -q fnm; fnm env --use-on-cd --shell fish | source; end
+
+    # Modern replacements (eza for ls, ug for grep)
+    if type -q eza
+        alias ls='eza'
+        alias ll='eza -l --icons=auto --group-directories-first'
+        alias l.='eza -d .*'
+        alias l1='eza -1'
+    end
+
+    if type -q ug
+        alias grep='ug'
+        alias egrep='ug -E'
+        alias fgrep='ug -F'
+    end
 end
